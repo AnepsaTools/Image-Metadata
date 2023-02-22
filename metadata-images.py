c6f20b8a10e989f7241508a2a86fa712a0d64622
LuisCMry7 <jorgecasas.isc@gmail.com>
2023-02-22 09:05:45 -0600

    first commit

---
+++ metadata-images.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+# Read ours images from current directory
+image = Image.open('IMG20230222085746.jpg')
+
+# Get the exif data from the image
+exif_data = image._getexif()
+
+# Loop through the exif data and print it
+for tagId in exif_data:
+    # Get the tag name, instead of human unreadable tag id
+    tag = TAGS.get(tagId, tagId)
+    data = exif_data.get(tagId)
+    # Decode bytes 
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag:25}: {data}")
+
+
+
+
